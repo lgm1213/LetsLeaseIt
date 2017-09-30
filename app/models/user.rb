3130ea304ee597a09fe2acf8ce6207941743b7bc
lgm1213 <lgm1213@gmail.com>
2017-09-30 16:41:52 -0400

    changes to user model and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+  before_save :downcase_email
+  validates :username, presence: true, length: { maximum: 30 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: false  
+  has_secure_password
+
+
+private
+  def downcase_email
+    self.email = email.downcase
+  end
+
 end
