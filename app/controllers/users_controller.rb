da3a6e338c41f3d2a33d4db288bbf46b75eb4938
lgm1213 <lgm1213@gmail.com>
2017-09-30 16:59:15 -0400

    added Remember digest column to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,10 +28,11 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        log_in @user
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
-        format.html { render :new }
+        format.html { render :new, notce: 'User was not created, please try again.' }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
