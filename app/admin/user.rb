1aee03905f394fbd1a396c8ea9b624f9ccc271bf
lgm1213 <lgm1213@gmail.com>
2017-10-09 19:09:05 -0400

    styled form for listings

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,10 +1,6 @@
 ActiveAdmin.register User do
-<<<<<<< HEAD
-  filter :email
-=======
 	role_changeable
 	filter :email
->>>>>>> ActiveAdminRoles
   filter :username
   filter :created_at
 # See permitted parameters documentation:
@@ -14,19 +10,15 @@ ActiveAdmin.register User do
 #
 # or
 #
-<<<<<<< HEAD
 # permit_params do
 #   permitted = [:permitted, :attributes]
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
-=======
   permit_params do
     permitted = [:username, :email, :password_digest, :title, :remember_digest, :role, :company_id]
     params[:user][:password] = params[:user][:password_digest] if params[:user].present?
     permitted.push << :password
     permitted
   end
-
->>>>>>> ActiveAdminRoles
 end
