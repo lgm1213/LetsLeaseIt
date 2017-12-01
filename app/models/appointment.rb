30c7679d4574779f58497be43ff60dbdb15c1955
Shraddha Chaturvedi <shradsror@gmail.com>
2017-12-01 09:58:10 +0530

    fixed indentation

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -6,15 +6,15 @@ class Appointment < ApplicationRecord
   has_many :rented_units, dependent: :destroy
   
   def self.appointment_notification
-	  from_time = (Time.now - 4.minutes).strftime("%Y-%m-%d %H:%M:00 +0000")
-	  to_time = Time.now.strftime("%Y-%m-%d %H:%M:00 +0000")
-	  appointments  = Appointment.where('end_time BETWEEN ? AND ?', from_time, to_time)
-	  user = User.find_by(role: "super_admin")
+    from_time = (Time.now - 4.minutes).strftime("%Y-%m-%d %H:%M:00 +0000")
+    to_time = Time.now.strftime("%Y-%m-%d %H:%M:00 +0000")
+    appointments  = Appointment.where('end_time BETWEEN ? AND ?', from_time, to_time)
+    user = User.find_by(role: "super_admin")
     if appointments.present?
-	    appointments.each do |a|
-	      building = a.listing.building
-	      conversation = user.send_message(building.property_manager, "Appointment for Building: #{building.development_name} is Completed on: #{a.end_time} by realtor: #{a.realtor_name} phone: #{a.realtor_phone}", "Appointment Complete Notification").conversation rescue nil
-	    end
+      appointments.each do |a|
+        building = a.listing.building
+        conversation = user.send_message(building.property_manager, "Appointment for Building: #{building.development_name} is Completed on: #{a.end_time} by realtor: #{a.realtor_name} phone: #{a.realtor_phone}", "Appointment Complete Notification").conversation rescue nil
+      end
     end
   end
 end
