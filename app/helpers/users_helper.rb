86944d1fc8cbe9a610f0f7c3a5e6c6d9804eb99c
lgm1213 <lgm1213@gmail.com>
2017-12-07 17:27:12 -0500

    created helper method to show appointments and listings on user#show

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,4 +5,10 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&r=g&d=#{CGI.escape(default_url)}"
     image_tag(gravatar_url, alt: user.username, class: "gravatar img-circle")
   end
+
+  def company_info
+  	company_id = @user.company_id
+  	@company = Company.find(company_id)
+  	return @company
+  end
 end
