da00d07c6a492bc3061d26960ae1968eaf2460ba
Shraddha Chaturvedi <shradsror@gmail.com>
2017-12-12 19:42:34 +0530

    Limiting users to contact according to their role

--- app/helpers/conversations_helper.rb
+++ app/helpers/conversations_helper.rb
@@ -1,2 +1,8 @@
 module ConversationsHelper
+  def recipients_per_role
+    user = User.where(role: ["property_manager" ,"leasing_consultant"]) if current_user.role=="consumer"
+    user = User.where(role: "property_manager") if current_user.role=="realtor"
+    user = User.all if current_user.role!="consumer" && current_user.role!="realtor"
+    return user
+  end
 end
