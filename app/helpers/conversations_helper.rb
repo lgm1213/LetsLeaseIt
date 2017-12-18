5d171febc70e2aa5ceb297fb96e89a1b0dcd420b
Shraddha Chaturvedi <shradsror@gmail.com>
2017-12-18 11:25:41 +0530

    Adding rules for users to contact according to their role

--- app/helpers/conversations_helper.rb
+++ app/helpers/conversations_helper.rb
@@ -1,8 +1,9 @@
 module ConversationsHelper
   def recipients_per_role
-    user = User.where(role: ["property_manager" ,"leasing_consultant"]) if current_user.role=="consumer"
-    user = User.where(role: "property_manager") if current_user.role=="realtor"
-    user = User.all if current_user.role!="consumer" && current_user.role!="realtor"
+    leaseit_account_managers = User.where(role: "account_manager", company_id: 12)
+    user = User.where(role: ["property_manager" ,"leasing_consultant"]) + leaseit_account_managers if current_user.role=="consumer"
+    user = User.where(role: "property_manager") + leaseit_account_managers  if current_user.role=="realtor"
+    user = (User.where(company_id:  current_user.company_id) + leaseit_account_managers).uniq if current_user.role!="consumer" && current_user.role!="realtor"
     return user
   end
 end
