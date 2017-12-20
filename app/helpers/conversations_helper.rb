d3367c0592446c3b5c3b6fd02e68ce8d3dece431
lgm1213 <lgm1213@gmail.com>
2017-12-20 18:17:51 -0500

    edited user show view

--- app/helpers/conversations_helper.rb
+++ app/helpers/conversations_helper.rb
@@ -1,9 +1,9 @@
 module ConversationsHelper
   def recipients_per_role
     leaseit_account_managers = User.where(role: "account_manager", company_id: 12)
-    user = User.where(role: ["property_manager" ,"leasing_consultant"]) + leaseit_account_managers if current_user.role=="consumer"
+    user = User.where(role: ["property_manager" ,"leasing_consultant"]) + leaseit_account_managers if current_user.role=="renter"
     user = User.where(role: "property_manager") + leaseit_account_managers  if current_user.role=="realtor"
-    user = (User.where(company_id:  current_user.company_id) + leaseit_account_managers).uniq if current_user.role!="consumer" && current_user.role!="realtor"
+    user = (User.where(company_id:  current_user.company_id) + leaseit_account_managers).uniq if current_user.role!="renter" && current_user.role!="realtor"
     return user
   end
 end
