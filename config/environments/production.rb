b98b70fcc570040e61b78f1f2c61289d135a014e
lgm1213 <lgm1213@gmail.com>
2017-12-27 20:19:51 -0500

    update gmail email settings after allowing less secure app settings in gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,17 +91,16 @@ Rails.application.configure do
 
   # SMTP Settings for Sending Email
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+   address: 'smtp.gmail.com',
+   port: 587,
+   domain: 'letsleaseit.com',    
+   user_name: ENV['GMAIL_USER'],
+   password: ENV['GMAIL_PW'],
+   authentication: 'login',
+   enable_starttls_auto: true  }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-   :address              => "smtp.gmail.com",
-   :port                 => 587,
-   :domain               => 'letsleaseit.com',    
-   :user_name            => ENV.fetch('GMAIL_USER'),
-   :password             => ENV.fetch('GMAIL_PW'),
-   :authentication       => "login",
-   :enable_starttls_auto => true
-  }
 
   config.paperclip_defaults = {
     storage: :s3,
